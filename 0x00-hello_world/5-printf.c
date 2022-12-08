526d4d994ffc57cabdc3e7ce33a66b9972830fe6
Omiko <amanwachiteresa@gmail.com>
2022-12-08 12:48:34 -0800

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 nclude <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
