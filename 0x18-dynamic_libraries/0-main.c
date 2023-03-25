cca4e2393db45d1b1eba35149eb8920853333b90
TeeOmyko <amanwachiteresa@gmail.com>
2023-03-24 17:23:41 -0700

    updated task 0

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -4,10 +4,10 @@
 /**
  * main - check the code
  *
- * Return: Always return 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("%d\n", _strlen("My Dyn Lib"));
-	return (0);
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (0);
 }
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,25 +1,31 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H
+#define _MAIN_H
 
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+extern int _putchar(char c);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int _abs(int n);
+extern int _isupper(int c);
+extern int _isdigit(int c);
+extern int _strlen(char *s);
+extern void _puts(char *s);
+extern char *_strcpy(char *dest, char *src);
+extern int _atoi(char *s);
+extern char *_strcat(char *dest, char *src);
+extern char *_strncat(char *dest, char *src, int n);
+extern char *_strncpy(char *dest, char *src, int n);
+extern int _strcmp(char *s1, char *s2);
+extern char *_memset(char *s, char b, unsigned int n);
+extern char *_memcpy(char *dest, char *src, unsigned int n);
+extern char *_strchr(char *s, char c);
+extern unsigned int _strspn(char *s, char *accept);
+extern char *_strpbrk(char *s, char *accept);
+extern char *_strstr(char *haystack, char *needle);
+
+extern int add(int a, int b);
+extern int sub(int a, int b);
+extern int mul(int a, int b);
+extern int div(int a, int b);
+extern int mod(int a, int b);
 
 #endif
